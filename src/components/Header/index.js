498a0bb336652ded2b35f95c6abd6a6cbb1b62fc
Darren Sarra <adriansarra18@gmail.com>
2020-06-13 00:49:18 -0400

    Started the index.js file for the header.

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return(
+    <div className="header-div">
+      <header>
+        <div className="header-left">
+          <button className="menu">MENU</button>
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default Header;
