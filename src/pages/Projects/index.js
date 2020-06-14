3f9b25e65b470a35e9ac5816abffa04feac04a67
Darren Sarra <adriansarra18@gmail.com>
2020-06-14 13:10:53 -0400

    Started a Projects page with a header and a footer.

---
+++ src/pages/Projects/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
+function Projects() {
+  return(
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  )
+};
+
+export default Projects;
