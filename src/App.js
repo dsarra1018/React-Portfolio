742a53cba26c70170c20e7904407b4c0fd019347
Darren Sarra <adriansarra18@gmail.com>
2020-06-14 13:29:01 -0400

    Setup router.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
 function App() {
   return(
-    <div>
-      <Header />
-      <Footer />
-    </div>
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/projects" component={Projects} />
+      <Route exact path="/contact" component={Contact} />
+    </Router>
   )
 };
 
