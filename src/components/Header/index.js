dfbce1f65ec608d14b2d3738a20a1c0780df13a2
Darren Sarra <adriansarra18@gmail.com>
2020-06-14 00:45:39 -0400

    Adjusted the Header component's font size.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,46 @@
-import React from "react";
+import React, {Component} from "react";
 import "./style.css";
 
-function Header() {
-  return(
-    <div className="header-div">
-      <header>
-        <div className="header-left">
-          <button className="menu">MENU</button>
-        </div>
-      </header>
-    </div>
-  )
+class Header extends Component {
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      visible: false
+    };
+
+    this.toggleMenu = this.toggleMenu.bind(this);
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+  }
+
+  toggleMenu() {
+    this.setState({
+      visible: !this.state.visible
+    });
+  }
+
+  handleMouseDown(e) {
+    this.toggleMenu();
+
+    e.stopPropagation();
+  }
+
+  render() {
+    return(
+      <div className="header-div">
+        <header>
+          <div className="header-left">
+            <button className="menu" onMouseDown={this.props.handleMouseDown}>MENU</button>
+          </div>
+          <div className="header-center">
+            <a href="/"><h1>DARREN ADRIAN SARRA</h1></a>
+          </div>
+          <div className="header-right"></div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default Header;
