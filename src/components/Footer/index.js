33433bc129850080d8c1f836ec9f04c3aca9d8e1
Darren Sarra <adriansarra18@gmail.com>
2020-06-14 00:46:35 -0400

    Started a Footer components.

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return(
+    <div className="footer-div">
+      <footer className="footer fixed-bottom">
+        <div className="copyright">
+          ©2020 DARREN SARRA
+        </div>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer;
