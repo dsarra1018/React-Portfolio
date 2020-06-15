c71f51bdaa56c6e52b3538de744b173e11a49fe9
Darren Sarra <adriansarra18@gmail.com>
2020-06-14 21:52:33 -0400

    Fixed styling issues with the About pages.

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../../components/Header";
-// import Footer from "../../components/Footer";
+import Footer from "../../components/Footer";
 import MainAbout from "../../components/MainAbout";
 
 function About() {
@@ -8,7 +8,7 @@ function About() {
       <div>
         <Header />
         <MainAbout />
-        {/* <Footer /> */}
+        <Footer />
       </div>
   )
 };
