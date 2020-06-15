a23b836cb4be49b993d5e649b63ab3657f59b934
Darren Sarra <adriansarra18@gmail.com>
2020-06-15 01:17:57 -0400

    Added forms inside the contact page.

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import Contacts from "../../components/Contacts";
 
 function Contact() {
   return(
     <div>
       <Header />
+      <Contacts />
       <Footer />
     </div>
   )
