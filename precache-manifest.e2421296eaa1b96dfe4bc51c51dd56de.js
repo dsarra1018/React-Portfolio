349866987aed1542ae16b4075684be3a098344d9
Darren Sarra <adriansarra18@gmail.com>
2020-06-15 20:01:53 -0400

    Updates

---
+++ precache-manifest.e2421296eaa1b96dfe4bc51c51dd56de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "878a696bf1889c45fb880a1bea4bbc35",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "9b2888e31becf7c6d028",
+    "url": "/my-app/static/css/main.0914202a.chunk.css"
+  },
+  {
+    "revision": "445359a6a01fed082fd0",
+    "url": "/my-app/static/js/2.459ecd0c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.459ecd0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b2888e31becf7c6d028",
+    "url": "/my-app/static/js/main.1d052dcd.chunk.js"
+  },
+  {
+    "revision": "f7d01a7a1ace24afb64f",
+    "url": "/my-app/static/js/runtime-main.6bb17465.js"
+  },
+  {
+    "revision": "f527fd9df6ca1ee2a5f95162dc4e9138",
+    "url": "/my-app/static/media/darren_sarra.f527fd9d.jpg"
+  }
+]);
